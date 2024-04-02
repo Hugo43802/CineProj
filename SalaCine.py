c1c1a601d217c28f66536d44e7a602601a8b37bc
Hugo Vega <76787314+Hugo43802@users.noreply.github.com>
2024-04-02 16:45:41 -0500

    Update SalaCine.py

--- SalaCine.py
+++ SalaCine.py
@@ -5,12 +5,19 @@ una sala con muchos objetos silla
 import Silla as file_silla
 
 class SalaCine:
+    '''
+    Constructor que genera las instancias que se van a tener en cuenta
+    '''
     def __init__(self, filas=0, columnas=0):
         self.filas = filas
         self.columnas = columnas
         self.mapa_sala = []
 
     def crear_sala(self):
+        '''
+        Función que genera la sala con las filas y columnas ingresadas por el usuario
+        Además, establece la numeración A1, A2...
+        '''
         for cont_row in range (1,self.filas+1):
             letra_fila = chr(64+cont_row)
             for cont_col in range(1, self.columnas+1):
@@ -18,9 +25,20 @@ class SalaCine:
                 self.mapa_sala.append(file_silla.Silla(pos_silla))
 
     def get_sala(self):
+        '''
+        Retorna el mapa de la sala creado
+        '''
         return self.mapa_sala
     
     def mostrarMapaSillas(self):
+        '''
+        Esta función realiza la muestra del mapa de las sillas generadas
+        por el usuario
+        
+        Realiza la consulta de qué sillas están reservadas, 
+        si está reservada va generar una salida [A1], si no, estará 
+        de manera general A1
+        '''
         print("\nMapa de la Sala:")
         for silla in self.mapa_sala:
             if silla.consultar_reserva():

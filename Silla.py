4baaa4371edfb493daf4f49136d928177bf945cc
Hugo Vega <76787314+Hugo43802@users.noreply.github.com>
2024-04-02 16:48:24 -0500

    Update Silla.py

--- Silla.py
+++ Silla.py
@@ -4,20 +4,20 @@ para los set se utilizará la reserva ya sea como true o false
 y los get devuelven el estado de la silla
 '''
 class Silla:
-
+    #En el constructor se inicia la posición de la silla
     def __init__(self, pos_Silla):
         self.pos_Silla = pos_Silla
         self.reserva = False
-    
+    #Este es el set que modifica la reserva
     def reservar(self):
         self.reserva = True
-
+    #Set que cancela la reserva
     def cancelar_reserva(self):
         self.reserva = False
-    
+    #Get que consulta en qué estado está la reserva
     def consultar_reserva(self):
         return self.reserva
-    
+    #Get que deuvleve la posición de la silla
     def mostrar_pos_silla(self):
         return self.pos_Silla
 
